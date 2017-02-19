119af676114b3ebe624244fd746f06ea97849411
objlong007 <466258187@qq.com>
2017-02-19 13:45:50 +0800

    express搭建访问接口，加载socket.io通讯

---
+++ src/server/server.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import {Server} from 'http'
+
+var app = express()
+var http = Server(app)
+
+// 配置
+var rootPath = require('path').normalize(__dirname + '/../..');
+app.set('views', __dirname + 'views');
+app.set('view engine', 'ejs');
+app.use(express.static(rootPath + '/public'));
+
+var io = require('socket.io', http);
+
+io.on('connection', (socket) => {
+  console.log('connected');
+  socket.on('disconnect', () => {
+    console.log('disconnected');
+  })
+})
+
+app.get('/', (req, res)=>{
+  res.send('111')
+});
+
+http.listen(3000, ()=>{
+  console.log('listening on port 3000')
+})
